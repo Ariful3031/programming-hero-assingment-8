03f9ac16425653cf137b3f61ae38b4fd55068d6f
unknown <hero.ariful31@gmail.com>
2025-10-09 00:18:41 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>hello assingment 6</h1>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Pages/Apps/Apps.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Apps = () => {
+    return (
+        <div>
+            <h1>hello Apps router</h1>
+        </div>
+    );
+};
+
+export default Apps;
